489eaba227ce8cb50172c010a329f2fbf013de70
rramirea04 <ricardoramirez2991@gmail.com>
2025-06-05 23:10:18 -0500

    otra prueva

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -24,4 +24,5 @@ def tipos():
     return render_template('tipos.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
